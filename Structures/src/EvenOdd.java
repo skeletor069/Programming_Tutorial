42d6d9d802010811323ef5a8de3d8caed0d950c7
Tanveer Al Jami <dela7543@in.uni-weimar.de>
2017-10-18 16:41:35 +0200

    Odd program done

--- Structures/src/EvenOdd.java
+++ Structures/src/EvenOdd.java
@@ -10,6 +10,16 @@ public class EvenOdd {
 		//todo: The program takes a number as input.
 		//It then goes through all integer values till reaching that value and prints all the odd ones.
 		
+		System.out.println("Enter a digit : ");
+		int number = scan.nextInt();
+		
+		System.out.println("Odd numbers till " + number + " : ");
+		
+		for(int i = 0 ; i <= number ; i++){
+			if(i%2 == 1)
+				System.out.println(i);
+		}
+		
 	}
 	
 }
